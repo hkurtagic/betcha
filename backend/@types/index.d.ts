475cead92ac91e1cf08daa9aa91cab3578174a76
Nini3010 <stefan-wesely@gmx.at>
2025-04-11 16:11:41 +0200

    base backend config

---
+++ backend/@types/index.d.ts
@@ -0,0 +1,11 @@
+// to make the file a module and avoid the TypeScript error
+export {};
+
+declare global {
+    module Express {
+        interface Request {
+            //userID: number;
+            //returnXML: boolean;
+        }
+    }
+}
